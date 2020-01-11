f94a48a8faa4d76f2175abf3cdf45d9b612f2ff4
Jakub Sosnowski <jakub.sosnowski@microsoft.wsei.edu.pl>
2020-01-11 20:02:03 +0100

    Unit 6

--- 1_Zadania/obowiązkowe/6_constructor_function/js/app.js
+++ 1_Zadania/obowiązkowe/6_constructor_function/js/app.js
@@ -1,76 +1,92 @@
-//Zadanie 0
-function Basket(product, sum) {
-    this.product = product;
-    this.sum = sum;
+//6.0
+
+function Basket() {
+    this.products = [];
+    this.sum = null;
+};
+
+Basket.prototype.addProduct = function (name, price) {
+    this.products.push(name)
+    this.sum += price
 }
 
-function addProduct(name, price) {
-    this.name = name;
-    this.price = price;
-}
+Basket.prototype.showBasket = function () {
+    console.log(
+        'Produkty: ' + this.products + '.',
+        'Cena ' + this.sum + 'zł'
+    )
+};
 
-function showBasket() {}
 var aliceBasket = new Basket();
 aliceBasket.addProduct("pomidor", 10);
 aliceBasket.addProduct("arbuz", 40);
 aliceBasket.showBasket();
+
 var bruceBasket = new Basket();
 bruceBasket.addProduct("rice", 10);
-bruceBasket.addProduct("grzyby mun", 50);
+bruceBasket.addProduct("grzyby mun", 50);
 bruceBasket.addProduct("tofu", 20);
 bruceBasket.showBasket();
-//Zadanie 1
-//Zadanie 2
-var Calculator = function () {
-    this.history = [];
 
+//6.1
+
+var Robot = function (name) {
+    this.name = name;
+    this.isFunctional = true;
+}
+
+Robot.prototype.sayHi = function (toWho) {
+    if (this.isFunctional === true) {
+        console.log("Robot " + this.name + " greets " + toWho);
+    } else {
+        console.log("Robot " + this.name + "is broken");
+    }
+};
+
+Robot.prototype.changeName = function (newname) {
+    console.log("Robot " + this.name + "changes name to " + newname);
+    name = newname;
+};
+
+Robot.prototype.fixIt = function () {
+    this.isFunctional = true;
+    console.log("Robot " + this.name + "was fixed");
+};
+
+//6.2
+
+function Calculator() {
+    this.operations = [];
 };
 
 Calculator.prototype.add = function (num1, num2) {
-    this.result = num1 + num2;
-    this.history.push('added ' + num1 + ' to ' + num2 + ' got ' + this.result);
-
+    var result = num1 + num2;
+    this.operations.push('added ' + num1 + ' to ' + num2 + ' got ' + result);
+    return result;
 };
 
 Calculator.prototype.multiply = function (num1, num2) {
-    this.result = num1 * num2;
-    this.history.push('multiplied ' + num1 + ' by ' + num2 + ' got ' + this.result);
-
+    var result = num1 * num2;
+    this.operations.push('multiplied ' + num1 + ' with ' + num2 + ' got ' + result);
+    return result;
 };
 
-Calculator.prototype.substract = function (num1, num2) {
-    this.result = num1 - num2;
-    this.history.push('substracted ' + num2 + ' from ' + num1 + ' got ' + this.result);
-
+Calculator.prototype.subtract = function (num1, num2) {
+    var result = num1 - num2;
+    this.operations.push('subtracted ' + num1 + ' from ' + num2 + ' got ' + result);
+    return result;
 };
 
 Calculator.prototype.divide = function (num1, num2) {
-    this.result = num1 / num2;
-    this.history.push('divided ' + num1 + ' by ' + num2 + ' got ' + this.result);
-
+    var result = num1 / num2;
+    this.operations.push('divided ' + num1 + ' by ' + num2 + ' got ' + result);
+    return result;
 };
 
 Calculator.prototype.printOperations = function () {
-    console.log('Operations performed: ', this.history);
+    console.log(this.operations);
 };
 
 Calculator.prototype.clearOperations = function () {
-    this.history = [];
-    console.log('Operations performed: ', this.history);
+    this.operations = [];
 };
-
-var myCalc = new Calculator();
-myCalc.add(2, 4);
-myCalc.multiply(2, 4);
-myCalc.substract(5, 3);
-myCalc.divide(15, 3);
-myCalc.printOperations();
-myCalc.clearOperations();
-
-
-var myCalc2 = new Calculator();
-myCalc2.add(22, 41);
-myCalc2.multiply(12, 4);
-myCalc2.substract(15, 3);
-myCalc2.divide(21, 3);
-myCalc2.printOperations();
