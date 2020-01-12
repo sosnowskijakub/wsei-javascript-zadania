b7b871083d941e964fead6eaeecca52a6cb3e61f
Jakub Sosnowski <jakub.sosnowski@microsoft.wsei.edu.pl>
2020-01-12 11:51:34 +0100

    Unit 8

--- 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
+++ 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
@@ -1,6 +1,49 @@
 document.addEventListener("DOMContentLoaded", function () {
-    /*
-    Poniżej napisz kod rozwiązujący zadania. Odpowiedzi oddzielaj komentarzami.
-     */
 
+    //8.0 
+
+    var title = document.getElementsByClassName("title");
+
+    function getDataAnimation(elemts) {
+        var variable = elemts[0].dataset.animation;
+        console.log(variable);
+        return variable;
+    }
+    getDataAnimation(title);
+
+    //8.1
+
+    var ById = document.getElementById('home');
+    console.log(ById);
+
+    var querySelectorID = document.querySelector('#home');
+    console.log(querySelectorID);
+
+    var querySelector = document.querySelector('li[data-direction]');
+    console.log(querySelector);
+
+    var querySelectorBlock = document.querySelector(".block");
+    console.log(querySelectorBlock);
+
+    //8.2
+
+    var querySelectorAll = document.querySelectorAll('nav li');
+    console.log(querySelectorAll);
+    console.log(querySelectorAll.length);
+
+    var querySelectorAllDiv = document.querySelectorAll('div p');
+    console.log(querySelectorAllDiv);
+    console.log(querySelectorAllDiv.length);
+
+    var querySelectorAllArticle = document.querySelectorAll('article p');
+    console.log(querySelectorAllArticle);
+    console.log(querySelectorAllArticle.length);
+
+    //8.3
+
+    var querySelectorArticle = document.querySelector('article.first');
+    console.log(querySelectorArticle);
+
+    var querySelectorArticleH1 = querySelectorArticle.querySelectorAll("h1");
+    console.log(querySelectorArticleH1.length);
 });
